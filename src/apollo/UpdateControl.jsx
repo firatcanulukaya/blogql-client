d11b8df45eb7f7d34441e057f91538335e6fd62a
GreXLin85 <38141908+GreXLin85@users.noreply.github.com>
2022-05-20 16:09:16 +0300

    Subscription'dan gelen verinin cache'e eklenmesi * Yorum satırlarını inceleyebilirsin

--- src/apollo/UpdateControl.jsx
+++ src/apollo/UpdateControl.jsx
@@ -1,25 +1,42 @@
-import {useSubscription, useQuery, useApolloClient} from "@apollo/client";
-import {POST_SUB, GET_POSTS} from "./getGQL";
-import {useEffect} from "react";
+import { useSubscription, useApolloClient } from "@apollo/client";
+import { POST_SUB, GET_POSTS } from "./getGQL";
+import { useEffect } from "react";
 
 const UpdateControl = () => {
     const client = useApolloClient();
-    const {data} = useSubscription(POST_SUB);
+    const { data } = useSubscription(POST_SUB);
 
     useEffect(() => {
-        const {posts}  = client.readQuery({
+        if (data === undefined) return; /**
+        sub'dan veri gelmemişse başta undefined olarak çalışıyor
+        eğer undefined olarak cache'e yazılmaya çalışılırsa 
+        uygulama çöküyor ondan kontrol eklendi
+        */
+
+        // cache'den postları okuyoruz ki üstüne ekleme yapıp cache'e yazabilelim
+        const { posts } = client.readQuery({
             query: GET_POSTS,
         });
-
+        
+        // cache'den gelen postların sonuna yeni post ekliyoruz
         client.writeQuery({
             query: GET_POSTS,
             data: {
-                posts: [data.post, ...posts],
+                /* öncelikle cache'den gelen postları alıyoruz
+                   sonra yeni postu ekliyoruz*/
+                posts: [...posts, data.postCreation],
             }
         });
 
-        console.log(posts);
-    }, [data]);
+        /* 
+        Buradaki iki dependecy'i yazmamızın sebebi
+        data - subscription'dan gelen verinin kontrolü
+        client - apollo client'ın kontrolü (değişirse veya bağlantı 
+                                            durumunda değişiklik 
+                                            olursa bu da güncellensin)
+
+        */
+    }, [data, client]);
 }
 
 export default UpdateControl;
