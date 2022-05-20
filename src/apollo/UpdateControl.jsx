5db1fcf5fd7a03fc2d76d5d9b9404281685e2c87
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-05-20 15:36:08 +0300

    apollo cache denemeleri

--- src/apollo/UpdateControl.jsx
+++ src/apollo/UpdateControl.jsx
@@ -1,15 +1,25 @@
-import {useSubscription} from "@apollo/client";
-import {POST_SUB} from "./getGQL";
+import {useSubscription, useQuery, useApolloClient} from "@apollo/client";
+import {POST_SUB, GET_POSTS} from "./getGQL";
 import {useEffect} from "react";
 
-
 const UpdateControl = () => {
+    const client = useApolloClient();
     const {data} = useSubscription(POST_SUB);
 
     useEffect(() => {
-        console.log(data);
-    }, [data]);
+        const {posts}  = client.readQuery({
+            query: GET_POSTS,
+        });
 
+        client.writeQuery({
+            query: GET_POSTS,
+            data: {
+                posts: [data.post, ...posts],
+            }
+        });
+
+        console.log(posts);
+    }, [data]);
 }
 
 export default UpdateControl;
