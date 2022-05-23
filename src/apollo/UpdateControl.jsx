1799a27346a2542f1940e8793f8b409d0033cb32
GreXLin85 <38141908+GreXLin85@users.noreply.github.com>
2022-05-23 11:11:22 +0300

    Daha iyi kontrollerin eklenmesi

--- src/apollo/UpdateControl.jsx
+++ src/apollo/UpdateControl.jsx
@@ -13,20 +13,42 @@ const UpdateControl = () => {
         uygulama çöküyor ondan kontrol eklendi
         */
 
-        // cache'den postları okuyoruz ki üstüne ekleme yapıp cache'e yazabilelim
-        const { posts } = client.readQuery({
-            query: GET_POSTS,
-        });
-        
-        // cache'den gelen postların sonuna yeni post ekliyoruz
-        client.writeQuery({
-            query: GET_POSTS,
-            data: {
-                /* öncelikle cache'den gelen postları alıyoruz
-                   sonra yeni postu ekliyoruz*/
-                posts: [...posts, data.postCreation],
-            }
-        });
+        try {
+            /**
+             * işlemleri try bloğunun içine yazmamızın sebebi eğer 
+             * önceden cache'de postlarla ilgili veri yoksa tüm ilerleme uçmasın diye
+             * - catch bloğunu inceleyin
+             *  */
+
+
+            // cache'den postları okuyoruz ki üstüne ekleme yapıp cache'e yazabilelim
+            const { posts } = client.readQuery({
+                query: GET_POSTS,
+            });
+
+            // cache'den gelen postların sonuna yeni post ekliyoruz
+            client.writeQuery({
+                query: GET_POSTS,
+                data: {
+                    /* öncelikle cache'den gelen postları alıyoruz
+                       sonra yeni postu ekliyoruz*/
+                    posts: [...posts, data.postCreation],
+                }
+            });
+        } catch (error) {
+            /**
+             * eğer cachede veri bulunmamışsa ve hata verilmişse buraya geliniyor
+             * ve direkt cache'e post verisi ekleniyor bu sayede sorunlar çözülmüş oluyor
+             *  */
+
+            client.writeQuery({
+                query: GET_POSTS,
+                data: {
+                    posts: [data.postCreation],
+                }
+            });
+        }
+
 
         /* 
         Buradaki iki dependecy'i yazmamızın sebebi
