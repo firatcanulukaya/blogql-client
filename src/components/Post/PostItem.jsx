89b5e164a42fb1eb8f66afa05df64e06530b4c1f
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-05-25 15:46:36 +0300

    postlar ve post ayrımı

---
+++ src/components/Post/PostItem.jsx
@@ -0,0 +1,23 @@
+import {useQuery} from "@apollo/client";
+import {GET_POSTS} from "../../apollo/getGQL";
+
+const PostItem = () => {
+    const {loading, error, data} = useQuery(GET_POSTS);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return (
+        <>
+            {
+                data?.posts?.map(post => (
+                    <div key={post.id} className="card">
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                ))
+            }
+            </>
+    )
+}
+
+export default PostItem;
--- src/components/Post/Posts.jsx
+++ src/components/Post/Posts.jsx
@@ -1,31 +1,13 @@
-import {useNavigate} from "react-router-dom"
-import {useQuery} from "@apollo/client";
-import {GET_POSTS, CURRENT_USER} from "../../apollo/getGQL";
 import PostItem from "./PostItem";
+import UserCard from "../UserCard";
 
-const Blogs = () => {
-    const navigate = useNavigate();
-    const {data: userData, error: userError, loading: userLoading} = useQuery(CURRENT_USER);
-
-    if (userLoading) return <p>Loading...</p>;
-    if (userError) return navigate('/');
-
+const Posts = () => {
     return (
         <div style={{display: "flex", gap: "2rem", flexDirection: "column", padding: "20px"}}>
-
+            <UserCard/>
             <PostItem/>
-
-            <div className="user">
-                <h1>Welcome {userData?.currentUser?.username}</h1>
-                <p>ID: {userData?.currentUser?.id}</p>
-                <button className="logoutBtn" onClick={() => {
-                    localStorage.removeItem("token");
-                    navigate('/')
-                }}>Logout
-                </button>
-            </div>
         </div>
     )
 }
 
-export default Blogs;
+export default Posts;
